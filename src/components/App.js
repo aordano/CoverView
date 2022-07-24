4ede374b38dba8bcbff12b98c2a459c5616d7375
Ágata Ordano <aordano@protonmail.com>
2022-07-24 20:08:06 -0300

    refactor(app): remove superfluous stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,16 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Editor from './Editor';
-import Home from './Home'
-import Faq from './Faq';
+import Editor from "./Editor";
 
 const App = () => {
-
     return (
         <BrowserRouter>
             <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/editor" element={<Editor />} />
-                <Route exact path="/faq" element={<Faq />} />
+                <Route exact path="/" element={<Editor />} />
             </Routes>
         </BrowserRouter>
     );
-
-}
+};
 
 export default App;
